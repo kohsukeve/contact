5cbfe7fce26b42cbc3b47a4f5260fc5f4df9302e
kohsukeve <kohsukeve@yahoo.co.jp>
2024-01-19 10:18:01 +0900

    aaaa

---
+++ src/app/api/hello/route.ts
@@ -0,0 +1,10 @@
+import { NextResponse } from "next/server"
+
+
+// export async function GET() {
+//     return NextResponse.json("Hello!")
+// }
+
+export async function GET() {
+    return NextResponse.json({name:"Hello!"})
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import ContactForm from "./components/ContactForm"
+
+
+const page = () => {
+  return (
+    <main className="flex grow flex-col items-center justify-center">
+      <div className="space-y-4">
+        <h1 className="text-4xl">Contact Us</h1>
+        <ContactForm />
+      </div>
+    </main>
+  )
+}
+
+export default page
